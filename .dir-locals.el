f72ad572bce0fbd8fefdbf5c2efbb4b56bdb0c04
Sergey Lilo <svlilo@yandex.ru>
2024-08-29 04:49:12 +0600

    init

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((ca65-mode . ((compile-command . "C:\\cc65\\bin\\cl65.exe --verbose --target nes -o floating6502.nes floating6502.s"))))
---
+++ floating6502.s
@@ -0,0 +1,32 @@
+;;; -*- mode:ca65; coding:utf-8 -*-
+.segment "HEADER"
+  ; .byte "NES", $1A      ; iNES header identifier
+  .byte $4E, $45, $53, $1A
+  .byte 2               ; 2x 16KB PRG code
+  .byte 1               ; 1x  8KB CHR data
+  .byte $01, $00        ; mapper 0, vertical mirroring
+
+.segment "VECTORS"
+  ;; When an NMI happens (once per frame if enabled) the label nmi:
+  .addr nmi
+  ;; When the processor first turns on or is reset, it will jump to the label reset:
+  .addr reset
+  ;; External interrupt IRQ (unused)
+  .addr 0
+
+; "nes" linker config requires a STARTUP section, even if it's empty
+.segment "STARTUP"
+
+; Main code segment for the program
+.segment "CODE"
+.segment "CHARS"
+
+reset:
+  nop
+  lda #$17
+  nop
+
+nmi:
+  lda #$19
+  nop
+  rti
