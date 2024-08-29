854ba6284a37aaf0b0848a3595a855327147e125
Sergey Lilo <svlilo@yandex.ru>
2024-08-29 07:45:45 +0600

    move code to babel src blocks

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((ca65-mode . ((compile-command . "C:\\cc65\\bin\\cl65.exe --verbose --target nes -o floating6502.nes floating6502.s"))))
